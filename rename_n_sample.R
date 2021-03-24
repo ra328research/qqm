632a365a107ed5747ce8b043b1db78e6c8a8b8e9
ra328research <55540280+ra328research@users.noreply.github.com>
2021-03-24 19:02:36 +0000

    Add files via upload

---
+++ rename_n_sample.R
@@ -0,0 +1,29 @@
+
+data <- read_csv("https://raw.githubusercontent.com/ra328research/qqm/main/qqm_labreport_dataset.csv") 
+
+# candidate number as seed
+RNGkind(sample.kind = "Rejection")
+set.seed(cand_no)
+
+# function to transform dot separation to camel format
+dot.to.camel <- function(x) {
+  x <- sub('^(\\w?)', '\\U\\1', x, perl=T)
+  x <- gsub('\\.(\\w?)', '\\U\\1',x, perl=T)
+  return(x)
+}
+
+#create matrix of names
+orig_names <- names(data)
+UPPER_NAMES <- toupper(names(data))
+dot.names <- gsub('\\_', '.', names(data))
+UPPER.DOT.NAMES <- toupper(dot.names)
+camelNames <- dot.to.camel(dot.names)
+data_names <- cbind(orig_names, UPPER_NAMES, dot.names, UPPER.DOT.NAMES, camelNames)
+
+#assign new names
+names(data) <- data_names[,sample(ncol(data_names), 1)]
+
+
+#random subset of rows - between 7 and 9 smaller than the whole dataset
+data <- data[sample(c(1:nrow(data)), 
+                    size = (nrow(data) - sample(7:9, 1))),]
